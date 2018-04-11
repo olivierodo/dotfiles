8ef81e001aafd3b3dddccb59beae406d2d417ce2
olivier rodomond <rodomond.o@gmail.com>
2018-04-11 12:56:55 +0700

    add iterm2 settings

--- scripts/install-mac.sh
+++ scripts/install-mac.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 echo "Installation special Mac"
+
+echo "setup iterm2"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
